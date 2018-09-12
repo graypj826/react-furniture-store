27d149a6b9b309737eaacb27ecedeaf565cfae9f
Patrick <graypj826@gmail.com>
2018-09-12 16:01:41 -0600

    imported store container

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Routes from './Auth/routes';
 import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import StoreContainer from '../Components/StoreContainer/storeContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
