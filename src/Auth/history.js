ba56fab5827567a68392320239618b013e0bfe53
Patrick <graypj826@gmail.com>
2018-09-12 16:21:02 -0600

    commenting out auth

--- src/Auth/history.js
+++ src/Auth/history.js
@@ -1,5 +1,5 @@
-import createHistory from 'history/createBrowserHistory';
+// import createHistory from 'history/createBrowserHistory';
 
-export default createHistory({
-  basename: process.env.NODE_ENV === 'development' ? '' : '/reactivesearch-auth0-example'
-});
+// export default createHistory({
+//   basename: process.env.NODE_ENV === 'development' ? '' : '/reactivesearch-auth0-example'
+// });
